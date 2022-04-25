c192cdcdac9f426f4cf49c5a048660bb2c8a40af
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-04-24 19:21:56 -0700

    kl

---
+++ js/site.js
@@ -0,0 +1,12 @@
+
+<script>
+// Open and close sidebar
+function openNav() {
+ document.getElementById("mySidebar").style.width = "60%";
+ document.getElementById("mySidebar").style.display = "block";
+}
+
+function closeNav() {
+ document.getElementById("mySidebar").style.display = "none";
+}
+</script>
